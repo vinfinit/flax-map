b3c122939290e5c9ac20bfd77cc116fba72105d1
vinfinit <artemenko.vladimi@gmail.com>
2018-02-22 21:49:37 +0300

    + france cooperative

--- src/build.js
+++ src/build.js
@@ -20,32 +20,33 @@ const locationList = [
   ]
   , [create_address('COOPÉRATIVE DE TEILLAGE DU PLATEAU DU NEUBOURG,1 Route de Coquerel, 27110 Crosville-la-Vieille')]
   , [create_address('Coopérative Agricole LInière de la Région d’Abbeville (CALIRA), 18 Route départementale, 80140 Martainneville, Somme')]
-  , [create_address('Coopérative Agricole Linière du Nord de Caen, Rue des Buissons, 14610 Villons-les-Buissons')]
-  , [create_address('SOC Cooperative Agricole L.A. Linière, 73 Route de Looberghe, 59630 Bourbourg')]
-  // , [create_address('Coopérative de Linière FONTAINE le dun-CANY, Saint Pierre Le Viger, 76740 Seine-Maritime ')]
+  , [create_address('Coopérative Agricole Linière du Nord de Caen, Rue des Buissons, 14610 Villons-les-Buissons', [49.233856, -0.413225])]
+  , [create_address('SOC Cooperative Agricole L.A. Linière, 73 Route de Looberghe, 59630 Bourbourg', [50.930902, 2.225995])]
+  , [create_address('Coopérative de Linière FONTAINE le dun-CANY, Saint Pierre Le Viger, 76740 Seine-Maritime', [49.828339, 0.851765])]
   , [create_address('COOPÉRATIVE DE TEILLAGE DE LIN DU VERT GALANT, 54 Route du Vert Galant, 76690 Saint-André-sur-Cailly')]
-  , [create_address('COOPERATIVE AGRICOLE Lin 2000 SCA, 20 Avenue Saget, 60210 Grandvilliers (там же - Cuma Liniere de Grandvilliers)')]
-  , [create_address("Coopérative Agricole Linière, Rue de l'Étoile, 14630 Cagny")]
-  , [create_address('OPALIN SCA, Route nationale, 62770 Le Parcq')]
+  , [create_address('COOPERATIVE AGRICOLE Lin 2000 SCA, 20 Avenue Saget, 60210 Grandvilliers', [49.6592994,1.9339162])]
+  , [create_address("Coopérative Agricole Linière, Rue de l'Étoile, 14630 Cagny", [49.150687, -0.258760])]
+  , [create_address('OPALIN SCA, Route nationale, 62770 Le Parcq', [50.374262,2.064595])]
   , [
     create_address('Liniere De Bosc Nouvel S.A., Le Bosc Nouvel, 76690 Le Bocasse')
-    , create_address("Teillage Vandecandelaere S.A.S., 5 Rue de l'Église, 14540 Bourguébus")
+    , create_address("Teillage Vandecandelaere S.A.S., 5 Rue de l'Église, 14540 Bourguébus", [49.120013, -0.296232])
   ]
   , [create_address('Teillage bellet Cie GmbH (Teillage Bellet & Cie S.A.R.L.), Plaine du Château, 76210 Raffetot')]
   , [
     create_address('Van Robaeys Frères, 83 Rue Saint-Michel, 59122 Killem')
-    , create_address("Van Robaeys Frères, Fortel-en-Artois 62270")
+    , create_address("Van Robaeys Frères Fortel-en-Artois 62270", [50.271365, 2.231550])
   ]
   , [create_address('Etablissements Devogele S.A.S., Le Buisson, 77120 Chailly-en-Brie')]
-  , [create_address('Vanhersecke Frères S.A.R.L, Barrière Française, 59143 Millam')]
-  , [create_address('Teillage de Saint Martin S.A.S, Centre Bourg, 27300 Saint-Martin-du-Tilleul')]
-  , [create_address('Linière de Saint Martin, 113 Rue du Puits, 27300 Saint-Martin-du-Tilleul')]
-  , [create_address('DeCock S.A., 1 Rue de la Cartonnerie, 59122 Hondschoote')]
-  , [create_address('Jean Decock S.A., 10 Route de Looweg, 59380 Quaëdypre')]
+  , [create_address('Vanhersecke Frères S.A.R.L, Barrière Française, 59143 Millam', [50.834368,2.277426])]
+  , [create_address('Novalin France, Chemin Départemental la Barrière Française, 59143 Millam', [50.8345196,2.2784653])]
+  , [create_address('Teillage de Saint Martin S.A.S, Centre Bourg, 27300 Saint-Martin-du-Tilleul', [49.1105738, 0.5287496])]
+  , [create_address('Linière de Saint Martin, 113 Rue du Puits, 27300 Saint-Martin-du-Tilleul', [49.094317, 0.587164])]
+  , [create_address('DeCock S.A., 1 Rue de la Cartonnerie, 59122 Hondschoote', [50.9905296, 2.5610947])]
+  , [create_address('Jean Decock S.A., 10 Route de Looweg, 59380 Quaëdypre', [50.9513724, 2.4298771])]
   , [create_address('Société Michel Dewynter SAS, 468 Route de Bourbourg, 59285 Rubrouck')]
   , [create_address('Liniere Du Ressault - Etablis Lamerant S.A., Rue Alexandre Duval, 27110 Le Neubourg')]
   , [create_address('Teillage Brille-Lamerant, Rue Alexandre Duval, 27110 Le Neubourg')]
-  // , [create_address('Teillage de lin Lievin  (MADAME SIMONNE LIEVIN), 22 Grand’ Rue  62140 REGNAUVILLE Région: Nord Pas De Calais')]
+  , [create_address('Teillage de lin Lievin  (MADAME SIMONNE LIEVIN), 22 Grand’ Rue  62140 REGNAUVILLE Région: Nord Pas De Calais', [50.315851, 2.008090])]
 
   , [
     create_address('Чаусский Льнозавод ОАО')
